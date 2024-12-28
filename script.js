dfeabbb565285f82e6e81a2bbb21741b352f247f
ihsanqfpp <okzihsan956@gmail.com>
2024-12-28 21:23:39 +0500

    init

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const getcolor = () => {
-
+const getColor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215);
-const randomCode = "#" + randomNumber.toString(16).padStart(6, '0'); // Combine "#" with the hexadecimal string
-console.log(randomNumber, randomCode);
-document.body.style.backgroundColor = randomCode;
-document.getElementById("color").innerText = randomCode;
+    const randomCode = "#" + randomNumber.toString(16).padStart(6, '0');
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color").innerText = randomCode;
 
-navigator.clipboard.writeText(randomCode)
-}
-document.getElementById("btn").addEventListener(
-    "click",
-    getcolor
-)
-getcolor();
+    navigator.clipboard.writeText(randomCode).then(() => {
+        const notice = document.querySelector(".copy-notice");
+        notice.style.display = "block";
+        setTimeout(() => {
+            notice.style.display = "none";
+        }, 2000);
+    });
+};
+
+document.getElementById("btn").addEventListener("click", getColor);
+
+// Initial call
+getColor();
